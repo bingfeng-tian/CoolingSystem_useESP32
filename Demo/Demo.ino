9bb9a0c96046add6433360641744a272fa7a53a4
bing feng <tianf914@gmail.com>
2024-06-10 00:31:23 +0800

    temp and fan

--- Demo/Demo.ino
+++ Demo/Demo.ino
@@ -1,8 +1,51 @@
-void setup() {
-  Serial.begin(6900);
-  Serial.println("Ready");
-}
+#include "DHT.h"
 
-void loop() {
+#define DHTPIN 33
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+#define FAN_PIN 13
+#define CRITICAL_TEMP 45
+
+float t;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+  dht.begin();  //初始化DHT
   
+  pinMode(FAN_PIN, OUTPUT);
+
+} // setup()
+
+void loop()
+{
+  readTemp();
+
+  if(t > CRITICAL_TEMP) {
+    digitalWrite(FAN_PIN, HIGH);
+    delay(5000);
+    while(t > CRITICAL_TEMP-10){
+      readTemp();
+    }; //持續吹風直到降到安全溫度
+  }else{
+    digitalWrite(FAN_PIN, LOW);
+  }
+
+} // loop()
+
+
+void readTemp() {
+  float h = dht.readHumidity();   //取得濕度
+  t = dht.readTemperature();  //取得溫度C
+
+  // //顯示在監控視窗裡
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" *C ");
+  delay(1000);
 }
