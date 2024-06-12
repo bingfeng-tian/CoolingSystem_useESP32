fd4f37270f26653c05b865654c1259d188a37107
bingfeng-tian <153074378+bingfeng-tian@users.noreply.github.com>
2024-06-12 15:47:27 +0800

    Add files via upload

--- Demo/Demo.ino
+++ Demo/Demo.ino
@@ -1,19 +1,27 @@
 #include "DHT.h"
+#include <LiquidCrystal_I2C.h>  // 引用I2C序列顯示器的程式庫
+#include "web.h"
 
 #define DHTPIN 33
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
+#define CRITICAL_TEMP 30
 #define FAN_PIN 13
-#define CRITICAL_TEMP 45
 
-float t;
+// LCD顯示器
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // 設定模組位址0x27，以及16行, 2列的顯示形式
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  setupWiFi();
+  
+  lcd.init();       // 初始化lcd物件
+  lcd.backlight();  // 開啟背光
+
   Serial.println("DHTxx test!");
-  dht.begin();  //初始化DHT
+  dht.begin();      //初始化DHT
   
   pinMode(FAN_PIN, OUTPUT);
 
@@ -22,11 +30,12 @@ void setup()
 void loop()
 {
   readTemp();
+  web_loop();
 
-  if(t > CRITICAL_TEMP) {
+  if(temp > CRITICAL_TEMP) {
     digitalWrite(FAN_PIN, HIGH);
     delay(5000);
-    while(t > CRITICAL_TEMP-10){
+    while(temp > CRITICAL_TEMP-5){
       readTemp();
     }; //持續吹風直到降到安全溫度
   }else{
@@ -37,15 +46,23 @@ void loop()
 
 
 void readTemp() {
-  float h = dht.readHumidity();   //取得濕度
-  t = dht.readTemperature();  //取得溫度C
+  humi = dht.readHumidity();   //取得濕度
+  temp = dht.readTemperature();  //取得溫度C
+  lcd.setCursor(0,0);
+  lcd.print("humi:");
+  lcd.print(humi);
+  lcd.setCursor(0,1);
+  lcd.print("temp:");
+  lcd.print(temp);
+  lcd.print(char(223));
+  lcd.print("C");
 
   // //顯示在監控視窗裡
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.println(" *C ");
+  // Serial.print("Humidity: ");
+  // Serial.print(h);
+  // Serial.print(" %\t");
+  // Serial.print("Temperature: ");
+  // Serial.print(t);
+  // Serial.println(" *C ");
   delay(1000);
 }
