aaca35d7b5e11290a35210fbf52f78a4e47b571b
bingfeng-tian <tianf914@gmail.com>
2024-06-18 10:36:24 +0800

    0618 up file

--- Demo/Demo.ino
+++ Demo/Demo.ino
@@ -1,6 +1,5 @@
 #include "DHT.h"
 #include <LiquidCrystal_I2C.h>  // 引用I2C序列顯示器的程式庫
-#include "web.h"
 
 #define DHTPIN 33
 #define DHTTYPE DHT11
@@ -9,13 +8,15 @@ DHT dht(DHTPIN, DHTTYPE);
 #define CRITICAL_TEMP 30
 #define FAN_PIN 13
 
+float humi;
+float temp;
+
 // LCD顯示器
 LiquidCrystal_I2C lcd(0x27, 16, 2);  // 設定模組位址0x27，以及16行, 2列的顯示形式
 
 void setup()
 {
   Serial.begin(115200);
-  setupWiFi();
   
   lcd.init();       // 初始化lcd物件
   lcd.backlight();  // 開啟背光
@@ -30,7 +31,6 @@ void setup()
 void loop()
 {
   readTemp();
-  web_loop();
 
   if(temp > CRITICAL_TEMP) {
     digitalWrite(FAN_PIN, HIGH);
@@ -57,12 +57,5 @@ void readTemp() {
   lcd.print(char(223));
   lcd.print("C");
 
-  // //顯示在監控視窗裡
-  // Serial.print("Humidity: ");
-  // Serial.print(h);
-  // Serial.print(" %\t");
-  // Serial.print("Temperature: ");
-  // Serial.print(t);
-  // Serial.println(" *C ");
   delay(1000);
 }
